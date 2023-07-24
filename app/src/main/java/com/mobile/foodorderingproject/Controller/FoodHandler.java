22da14765cd1b9955fc6f0d2f74b2c590449c1ce
Kyan123Trung <138291264+Kyan123Trung@users.noreply.github.com>
2023-07-24 13:36:05 +0700

    FoodHandler

--- app/src/main/java/com/mobile/foodorderingproject/Controller/FoodHandler.java
+++ app/src/main/java/com/mobile/foodorderingproject/Controller/FoodHandler.java
@@ -1,4 +1,34 @@
 package com.mobile.foodorderingproject.Controller;
 
-public class FoodHandler {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class FoodHandler extends SQLiteOpenHelper {
+    private static final String DB_NAME = "qlsv";
+    @SuppressLint("SdCardPath")
+    private static final String PATH = "/data/data/com.mobile.foodorderingproject/database/FoodOrdering.db";
+    private static final String TABLE_NAME = "Food";
+    private static final String MAFOOD_COL = "MaFood";
+    private static final String TENFOOD_COL = "TenFood";
+    private static final String GIAFOOD_COL = "GiaFood";
+    public FoodHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DB_NAME, factory, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db = SQLiteDatabase.openDatabase(PATH, null,SQLiteDatabase.CREATE_IF_NECESSARY);
+        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" + MAFOOD_COL +" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, " + TENFOOD_COL + " TEXT NOT NULL UNIQUE, " + GIAFOOD_COL + " INTEGER NOT NULL)";
+        db.execSQL(sql);
+        db.close();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
